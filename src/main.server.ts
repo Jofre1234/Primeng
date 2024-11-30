6bbe9b6f9b6966eb5119f03408ec4ebf7c28f86d
Jofre1234 <jjm.bedon@yavirac.edu.ec>
2024-11-29 21:05:07 -0500

    Initial commit

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
+import { AppComponent } from './app/AppComponent.1';
 import { config } from './app/app.config.server';
+
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
